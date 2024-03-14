79a03a65108be00d37acf6c7d3877e37eee00c27
Andrej Benz <hello@benz.dev>
2024-03-14 19:35:13 +0100

    change version

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ import (
 	"github.com/diamondburned/gotk4/pkg/gtk/v4"
 )
 
-const VERSION = "0.0.10-git"
+const VERSION = "0.0.10"
 
 type Config struct {
 	Placeholder        string                 `json:"placeholder,omitempty"`

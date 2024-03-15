b272c5a9b37309553fe120ca6a15fd3e2f75a07a
Andrej Benz <hello@benz.dev>
2024-03-15 08:17:04 +0100

    bump version

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ import (
 	"github.com/diamondburned/gotk4/pkg/gtk/v4"
 )
 
-const VERSION = "0.0.12"
+const VERSION = "0.0.13-git"
 
 type Config struct {
 	Placeholder           string                 `json:"placeholder,omitempty"`

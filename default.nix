361ce432f56d3d1ba8c285bbe8b2a2b44209d4df
diniamo <diniamo69@gmail.com>
2024-03-15 16:54:12 +0100

    nix: dev commit

--- default.nix
+++ default.nix
@@ -1,8 +1,27 @@
-{buildGoApplication, lib}:
-
+{
+  buildGoApplication,
+  lib,
+  go,
+  pkg-config,
+  glibc,
+}:
 buildGoApplication {
   pname = "walker";
   version = lib.fileContents ./version.txt;
+
+  pwd = ./.;
   src = ./.;
   modules = ./gomod2nix.toml;
+  inherit go;
+
+  nativeBuildInputs = [pkg-config glibc];
+
+  meta = with lib; {
+    description = "Wayland-native application runner";
+    homepage = "https://github.com/abenz1267/walker";
+    license = licenses.mit;
+    maintainers = with maintainers; [diniamo];
+    platforms = platforms.linux;
+    mainProgram = "walker";
+  };
 }
--- shell.nix
+++ shell.nix
@@ -6,7 +6,6 @@
   pkg-config,
   gtk4-layer-shell,
 }:
-
 mkShell {
   packages = [
     (mkGoEnv {pwd = ./.;})

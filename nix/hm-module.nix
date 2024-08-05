71a7b95e41e12dbc58135623335852bddf99b173
diniamo <diniamo53@gmail.com>
2024-08-05 18:27:46 +0200

    nix(hm): adopt new theming system

--- nix/hm-module.nix
+++ nix/hm-module.nix
@@ -4,7 +4,31 @@ self: {
   pkgs,
   ...
 }: let
-  inherit (lib) mkEnableOption mkOption mkPackageOption types importJSON mkIf getExe;
+  inherit (lib) mkEnableOption mkOption mkPackageOption importJSON mkIf getExe mkForce mkMerge;
+  inherit (lib.types) bool nullOr submodule lines;
+
+  jsonFormat = pkgs.formats.json {};
+
+  themeType = submodule {
+    options = {
+      layout = mkOption {
+        inherit (jsonFormat) type;
+        default = {};
+        description = ''
+          The layout of the theme.
+
+          See <https://github.com/abenz1267/walker/wiki/Theming> for the full list of options.
+        '';
+      };
+
+      style = mkOption {
+        type = lines;
+        default = "";
+        description = "The styling of the theme, written in GTK CSS.";
+      };
+    };
+  };
+  themeName = "home-manager";
 
   cfg = config.programs.walker;
 in {
@@ -17,28 +41,49 @@ in {
       };
 
       runAsService = mkOption {
-        type = types.bool;
+        type = bool;
         default = false;
-        description = "Run as a service";
+        description = "Run walker as a service for faster startup times.";
       };
 
       config = mkOption {
-        type = types.attrs;
+        inherit (jsonFormat) type;
         default = importJSON ../config/config.default.json;
-        description = "Configuration";
+        description = ''
+          Configuration written to `$XDG_CONFIG_HOME/walker/config.json`.
+
+          See <https://github.com/abenz1267/walker/wiki/Basic-Configuration> for the full list of options.
+        '';
+      };
+
+      theme = mkOption {
+        type = nullOr themeType;
+        default = null;
+        description = "The custom theme used by walker. Setting this option overrides `config.theme`.";
       };
     };
   };
 
-  config = mkIf cfg.enable {
-    home.packages = [cfg.package];
+  config = mkIf cfg.enable (mkMerge [
+    {
+      home.packages = [cfg.package];
 
-    xdg.configFile."walker/config.json".text = mkIf (cfg.config != {}) (builtins.toJSON cfg.config);
+      xdg.configFile."walker/config.json".source = mkIf (cfg.config != {}) (jsonFormat.generate "walker-config.json" cfg.config);
 
-    systemd.user.services.walker = mkIf cfg.runAsService {
-      Unit.Description = "Walker - Application Runner";
-      Install.WantedBy = ["graphical-session.target"];
-      Service.ExecStart = "${getExe cfg.package} --gapplication-service";
-    };
-  };
+      systemd.user.services.walker = mkIf cfg.runAsService {
+        Unit.Description = "Walker - Application Runner";
+        Install.WantedBy = ["graphical-session.target"];
+        Service.ExecStart = "${getExe cfg.package} --gapplication-service";
+      };
+    }
+
+    (mkIf (cfg.theme != null) {
+      programs.walker.config.theme = mkForce themeName;
+
+      xdg.configFile = {
+        "walker/themes/${themeName}.json" = jsonFormat.generate "walker-themes-${themeName}.json" cfg.theme.layout;
+        "walker/themes/${themeName}.css" = jsonFormat.generate "walker-themes-${themeName}.css" cfg.theme.style;
+      };
+    })
+  ]);
 }

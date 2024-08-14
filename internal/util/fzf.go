b2e72b9409461154af4ef94401d06b5032ce462d
Andrej Benz <hello@benz.dev>
2024-08-14 07:49:22 +0200

    minor: consider start of match and not just match score

--- internal/util/fzf.go
+++ internal/util/fzf.go
@@ -11,5 +11,5 @@ func FuzzyScore(input, target string) float64 {
 	chars := util.ToChars([]byte(target))
 	res, _ := algo.FuzzyMatchV2(false, true, true, &chars, []rune(strings.ToLower(input)), true, nil)
 
-	return float64(res.Score)
+	return float64(res.Score - res.Start)
 }

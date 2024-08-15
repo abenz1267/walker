c5f829720a79ad975f5bdc45d0d47e1827d3f726
Andrej Benz <hello@benz.dev>
2024-08-15 21:51:17 +0200

    clipboard: re-copy copied text to let it persist even if source application gets closed

--- internal/modules/clipboard/clipboard.go
+++ internal/modules/clipboard/clipboard.go
@@ -191,6 +191,10 @@ func (c *Clipboard) watch() {
 			file := saveTmpImg(val)
 			e.Content = file
 			e.IsImg = true
+		} else {
+			cmd := exec.Command("wl-copy")
+			cmd.Stdin = strings.NewReader(e.Content)
+			cmd.Start()
 		}
 
 		c.entries = append([]util.Entry{itemToEntry(e)}, c.entries...)

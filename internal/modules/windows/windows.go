3907217eda5732d9cca042669cea46035068423d
Andrej Benz <hello@benz.dev>
2024-11-24 12:54:31 +0100

    windows: try to display app icon when possible

--- internal/modules/windows/windows.go
+++ internal/modules/windows/windows.go
@@ -1,12 +1,18 @@
 package windows
 
 import (
+	"bufio"
 	"context"
 	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/abenz1267/walker/internal/config"
 	"github.com/abenz1267/walker/internal/modules/windows/wlr"
 	"github.com/abenz1267/walker/internal/util"
+	"github.com/adrg/xdg"
 	"github.com/neurlang/wayland/wl"
 )
 
@@ -14,6 +20,7 @@ type Windows struct {
 	general   config.GeneralModule
 	entries   []util.Entry
 	functions []func()
+	icons     map[string]string
 }
 
 func (w Windows) General() *config.GeneralModule {
@@ -34,8 +41,65 @@ func (w *Windows) SetupData(cfg *config.Config, ctx context.Context) {
 		go wlr.StartWM(nil, nil)
 	}
 
+	w.icons = make(map[string]string)
+	w.GetIcons()
+
 	w.general.IsSetup = true
 	w.general.HasInitialSetup = true
+}
+
+func (w *Windows) GetIcons() {
+	dirs := xdg.ApplicationDirs
+
+	done := make(map[string]struct{})
+
+	for _, d := range dirs {
+		if _, err := os.Stat(d); err != nil {
+			continue
+		}
+
+		filepath.WalkDir(d, func(path string, info fs.DirEntry, err error) error {
+			if _, ok := done[info.Name()]; ok {
+				return nil
+			}
+
+			if !info.IsDir() && filepath.Ext(path) == ".desktop" {
+				file, err := os.Open(path)
+				if err != nil {
+					return err
+				}
+
+				defer file.Close()
+
+				scanner := bufio.NewScanner(file)
+
+				icon, class := "", ""
+
+				for scanner.Scan() {
+					if icon != "" && class != "" {
+						w.icons[class] = icon
+					}
+
+					line := scanner.Text()
+
+					if strings.HasPrefix(line, "StartupWMClass=") {
+						class = strings.TrimSpace(strings.TrimPrefix(line, "StartupWMClass="))
+						class = strings.ToLower(class)
+						continue
+					}
+
+					if strings.HasPrefix(line, "Icon=") {
+						icon = strings.TrimSpace(strings.TrimPrefix(line, "Icon="))
+						continue
+					}
+				}
+			}
+
+			done[info.Name()] = struct{}{}
+
+			return nil
+		})
+	}
 }
 
 func (w Windows) Entries(ctx context.Context, term string) []util.Entry {
@@ -48,6 +112,7 @@ func (w Windows) Entries(ctx context.Context, term string) []util.Entry {
 			Label:           v.Title,
 			Sub:             fmt.Sprintf("Windows: %s", v.AppId),
 			Searchable:      v.AppId,
+			Icon:            w.icons[v.AppId],
 			Categories:      []string{"windows"},
 			Class:           "windows",
 			Matching:        util.Fuzzy,

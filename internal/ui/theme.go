52f5f53c6615d9c0c0bae5c72bff88908853b264
fbird <info@nconnect.hu>
2025-03-22 21:30:30 +0100

    Replacing "sh" "-c" with "env" "-S"
    Replacing "sh" "-c" with "env" "-S"

--- internal/ui/theme.go
+++ internal/ui/theme.go
@@ -248,7 +248,7 @@ func setupBarTheme() {
 			controller.SetPropagationPhase(gtk.PropagationPhase(1))
 			controller.Connect("pressed", func(gesture *gtk.GestureClick, n int) {
 				if v.Module == "" && v.Exec != "" {
-					cmd := exec.Command("sh", "-c", wrapWithPrefix(v.Exec))
+					cmd := exec.Command("env", "-S", wrapWithPrefix(v.Exec))
 
 					cmd.SysProcAttr = &syscall.SysProcAttr{
 						Setpgid:    true,

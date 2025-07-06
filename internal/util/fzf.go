3b9f1944be9e462d351106c643e43b0ca7c73390
Andrej Benz <hello@benz.dev>
2025-07-06 17:53:38 +0200

    search: added smartcase to search, so case-sensitive when search contains upper-case letter

--- internal/util/fzf.go
+++ internal/util/fzf.go
@@ -1,22 +1,25 @@
 package util
 
 import (
-	"strings"
+	"slices"
+	"unicode"
 
 	"github.com/junegunn/fzf/src/algo"
 	"github.com/junegunn/fzf/src/util"
 )
 
 func FuzzyScore(input, target string) (float64, *[]int, int) {
+	runes := []rune(input)
 	chars := util.ToChars([]byte(target))
-	res, pos := algo.FuzzyMatchV2(false, true, true, &chars, []rune(strings.ToLower(input)), true, nil)
+	res, pos := algo.FuzzyMatchV2(slices.ContainsFunc(runes, unicode.IsUpper), true, true, &chars, runes, true, nil)
 
 	return float64(res.Score), pos, res.Start
 }
 
 func ExactScore(input, target string) (float64, *[]int, int) {
+	runes := []rune(input)
 	chars := util.ToChars([]byte(target))
-	res, pos := algo.ExactMatchNaive(false, true, true, &chars, []rune(strings.ToLower(input)), true, nil)
+	res, pos := algo.ExactMatchNaive(slices.ContainsFunc(runes, unicode.IsUpper), true, true, &chars, runes, true, nil)
 
 	return float64(res.Score), pos, res.Start
 }

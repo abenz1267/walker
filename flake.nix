ec690a5e5deb53a91e9c079fcffea492e58d457a
not-matthias <matthias@codspeed.io>
2025-08-24 16:17:12 +0200

    fixup: use elephant fork

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     systems.url = "github:nix-systems/default-linux";
-    elephant.url = "github:kagurazakei/elephant";
+    elephant.url = "github:not-matthias/elephant";
   };
 
   outputs = {
